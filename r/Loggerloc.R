73d4e241e7deb41d25fa40285f284c504a2ce094
sscolaro <sscolaro@tbep.org>
2023-05-25 16:33:16 -0400

    Updated to logger sites

--- r/Loggerloc.R
+++ r/Loggerloc.R
@@ -1,6 +1,19 @@
-##Generating random temp logger sites
+##Generating random temp logger sites:Summer 2023
 
-# Install and load packages
+# Install packages
+
+options(repos = c(
+  tbeptech = 'https://tbep-tech.r-universe.dev',
+  CRAN = 'https://cloud.r-project.org'))
+
+install.packages('tbeptools')
+install.packages("remotes")
+install.packages("rgdal")
+
+# install the most recent development version of EPA spsurvey from GitHub
+remotes::install_github("USEPA/spsurvey", ref = "main")
+
+# Load packages
 library(sf)
 library(tidyverse)
 library(mapview)
@@ -11,36 +24,29 @@ library(raster)
 library(here)
 library(spatstat)
 library(remotes)
-
-options(repos = c(
-  tbeptech = 'https://tbep-tech.r-universe.dev',
-  CRAN = 'https://cloud.r-project.org'))
-
-# install tbeptools
-install.packages('tbeptools')
 library(tbeptools)
-
-# Installing from GitHub requires you first install the remotes package
-install.packages("remotes")
-
-# install the most recent development version of EPA spsurvey from GitHub
-remotes::install_github("USEPA/spsurvey", ref = "main")
-# load the most recent development version from GitHub
 library(spsurvey)
+library(rgdal)
 
-#Load MBeck previous [sg analysis change data file](https://github.com/tbep-tech/seagrass-analysis) and Bay segment boundaries
-chgdat
-
+####Seagrass Changes for loss sites
+#Load [seagrass change data from 2020 to 2022](https://tbep-tech.github.io/seagrass-analysis/seagrasschange20202022.html) and Bay segment boundaries
+load(file = 'data/chgdat20202022.RData')
 
 #load seagrass management areas and Tampa Bay Segments
 sgmanagement
 tbseg
+
+## colors
+cols <- c('green4', 'tomato1')
+names(cols) <- c('gained', 'lost')
 
 # reproject layers
 prj4 <- '+proj=tmerc +lat_0=24.33333333333333 +lon_0=-82 +k=0.999941177 +x_0=200000.0001016002 +y_0=0 +ellps=GRS80 +to_meter=0.3048006096012192 +no_defs'
 sgmanagement <- sgmanagement %>% 
   st_transform(crs = prj4)
 tbseg<- tbseg %>% 
+  st_transform(crs = prj4)
+chgdat20202022 <- chgdat20202022 %>% 
   st_transform(crs = prj4)
 
 ##Filter boundaries to only include OTB
@@ -49,27 +55,32 @@ filt_dat <- tbseg %>%
 filt_dat
 
 #clip sg change data to only Old Tampa Bay segment
-sg_clip= st_intersection(chgdat,filt_dat)
+sg_clip= st_intersection(chgdat20202022,filt_dat)
 
-st_write(sg_clip, "OTBchgdat.shp")
-## colors
-cols <- c('green4', 'tomato1')
-names(cols) <- c('gained', 'lost')
+mapview(sg_clip, zcol = 'var', layer.name = 'Seagrass', col.regions = cols)
+st_write(sg_clip, "OTBchgdat20202022.shp")
+
+###Load bathy file, filter out all depths >2
+load(file = 'data/dem (1).RData')
+dem[dem[] < -2] <- NA
+
+mapview(sg_clip, zcol = 'var', layer.name = 'Seagrass', col.regions = cols)+
+  mapview(dem, layer.name = 'Depth (m)')
 
 ##Select a sample stratified by seagrass change area (https://cran.r-project.org/web/packages/spsurvey/vignettes/sampling.html#3_Stratified_sampling)
-stata_n<- c(gained = 3, lost=3)
+stata_n<- c(lost=4)
 strata_eqprob<- grts(sg_clip,n_base = stata_n, stratum_var = "var")
 sp_plot(strata_eqprob)
 
 #transform list of sites into a dataframe and project to prj4 for consistency
-sites<- as.data.frame(strata_eqprob$sites_base)
-write.csv(sites, "C:\\Users\\sscol\\OneDrive\\Desktop\\Logger\\otb-temp\\data\\sites_20220922.csv")
+Loss_sites<- as.data.frame(strata_eqprob$sites_base)
+write.csv(sites, "C:\\Users\\sscol\\OneDrive\\Desktop\\Logger\\otb-temp\\data\\Loss_sites_2023.csv")
 
-sites_geo<-st_as_sf(sites,coords = c("lon_WGS84","lat_WGS84"), crs=4326)
-sites_geo<- sites_geo %>% st_transform(crs = prj4)
+Loss_sites_geo<-st_as_sf(sites,coords = c("lon_WGS84","lat_WGS84"), crs=4326)
+Loss_sites_geo<- sites_geo %>% st_transform(crs = prj4)
 
 
 #Generate a map selected sites
 mapview(sg_clip, zcol = 'var', layer.name = 'Seagrass', col.regions = cols)+
-  mapview(filt_dat, layer.name= 'OTB',col.regions='black', alpha.regions=0)+
-  mapview(sites_geo, layer.name='statum', color = 'blue', alpha.regions=0)
+  mapview(dem, layer.name = 'Depth (m)')+
+  mapview(Loss_sites_geo, layer.name='statum', color = 'yellow', alpha.regions=0)
