e55001acd063789cbd3c43732bf6d8ca0a6752e5
fawda123 <mbafs2012@gmail.com>
2023-07-12 16:14:24 -0400

    add 2023 to metadata, temp data, and quarto

--- r/dat_proc.R
+++ r/dat_proc.R
@@ -11,22 +11,22 @@ gs4_deauth()
 
 # gdrive info ---------------------------------------------------------------------------------
 
-gdrive_pth <- 'https://drive.google.com/drive/folders/1MauA87CetbHCl1IcSolrv2Q8HRXoM9NL?usp=sharing'
+gdrive_pth <- 'https://drive.google.com/drive/folders/1MauA87CetbHCl1IcSolrv2Q8HRXoM9NL'
 
 fls <- drive_ls(gdrive_pth) %>% 
   filter(!grepl('\\.xlsx$', name))
-
 
 # site metadata -------------------------------------------------------------------------------
 
 metadat <- fls %>% 
   filter(grepl('OTB_TEMP_LOGGER_DATA', name)) %>% 
   pull(id) %>% 
-  read_sheet(na = c('none', 'NA'), col_types = 'ttnccnnnccccccc') %>% 
+  read_sheet(na = c('', 'NA'), col_types = 'DcDcccnnnnnnccccccc') %>% 
   clean_names %>% 
-  filter(site_id %in% c(1:6)) %>% 
   select(
+    deploy_date,
     logger = logger_id,
+    site = site_id,
     stratum, 
     lat, 
     long, 
@@ -36,34 +36,50 @@ metadat <- fls %>%
     macro_percent, 
     macro_species
   ) %>% 
-  filter(!is.na(logger)) %>% 
+  mutate(
+    yr = year(deploy_date), 
+    logger = sprintf('%04d', logger)
+  ) %>% 
+  filter(
+    (yr == 2022 & site %in% c(1, 2, 4, 5, 6)) |
+    yr == 2023
+    ) %>% 
   st_as_sf(coords = c('long', 'lat'), crs = 4326)
 
 save(metadat, file = here('data/metadat.RData'))
 
 # temp data -----------------------------------------------------------------------------------
 
+load(file = here('data/metadat.RData'))
+
+# logger, site lookup
+lkup <- metadat %>% 
+  st_set_geometry(NULL) %>% 
+  select(yr, site, logger) %>% 
+  unique()
+
 datfls <- fls %>% 
-  .[!grepl('OTB_TEMP_LOGGER_DATA|DATASHEETS', .$name),] 
+  .[!grepl('OTB_TEMP_LOGGER_DATA|DATASHEETS|^Avg', .$name),] 
 
 tempdat <- NULL
 for(i in 1:nrow(datfls)){
+  
+  cat(i,'\n')
   
   id <- datfls[i, ] %>% 
     pull(id)
   
   logger <- datfls[i, ] %>% 
-    pull(name) %>% 
-    gsub(' .*', '', .) %>% 
-    gsub('2145', '', .)
+    pull(name)
   
   out <- read_sheet(id) %>% 
     mutate(
       logger = logger, 
       elapsed = `Date-Time (EDT)` - min(`Date-Time (EDT)`)
     ) %>% 
-    filter(elapsed > 3600)
+    filter(elapsed > 3600) # remove first hour
   
+  names(out)[grepl('Temp', names(out))] <- 'tempc'
   tempdat <- bind_rows(tempdat, out)
   
 }
@@ -73,10 +89,15 @@ tempdat <- tempdat %>%
   select(
     logger, 
     datetime = date_time_edt, 
-    tempc = ch_1_temperature_c
+    tempc
   ) %>% 
   mutate(
-    datetime = force_tz(datetime, tzone = 'America/New_York')
-  )
+    datetime = force_tz(datetime, tzone = 'America/New_York'),
+    yr = year(datetime), 
+    logger = gsub(' .*', '', logger), 
+    logger = gsub('2145', '', logger), 
+    logger = gsub('^.*_.*_(.*$)', '\\1', logger)
+  ) %>% 
+  left_join(lkup, by = c('yr', 'logger'))
 
 save(tempdat, file = here('data/tempdat.RData'))
